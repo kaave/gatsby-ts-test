6336d27aab22089830ad6698621101c381f42ed1
kaave <junkjunctions@gmail.com>
2019-12-13 01:16:27 +0900

    add: husky & lint-staged

---
+++ lint-staged.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  '*.{js,ts,tsx}': ['eslint --fix', 'git add'],
+  // '*.scss': ['stylelint --fix', 'git add'],
+  '*.json': ['prettier --write', 'git add'],
+  'package.json': ['sort-package-json', 'git add'],
+};
