2c978afcb514770ef326660058c62a29295ab5dd
kaave <junkjunctions@gmail.com>
2019-12-14 00:21:53 +0900

    add: cspell

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,6 +1,6 @@
 module.exports = {
-  '*.{js,ts,tsx}': ['eslint --fix', 'git add'],
-  // '*.scss': ['stylelint --fix', 'git add'],
+  '*.{js,ts,tsx}': ['eslint --fix', 'cspell', 'git add'],
+  // '*.scss': ['stylelint --fix', 'cspell', 'git add'],
   '*.json': ['prettier --write', 'git add'],
   'package.json': ['sort-package-json', 'git add'],
 };
