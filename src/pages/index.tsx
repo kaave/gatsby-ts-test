12e0df1ec26abe5cf5b98dd0795b0f0ba6f53147
kaave <junkjunctions@gmail.com>
2019-12-14 12:09:36 +0900

    fix: use graphQL on page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,27 +1,34 @@
 import * as React from 'react';
+import { graphql } from 'gatsby';
 import Link from 'gatsby-link';
 
 import { Layout } from '~/layouts/default';
 import styles from './index.module.scss';
+import { IndexQuery } from '../../types/graphql-types';
 
-// Please note that you can use https://github.com/dotansimha/graphql-code-generator
-// to generate all types from graphQL schema
+export const pageQuery = graphql`
+  query Index {
+    site {
+      siteMetadata {
+        title
+        siteUrl
+      }
+    }
+  }
+`;
+
 type Props = {
-  data: {
-    site: {
-      siteMetadata: {
-        title: string;
-      };
-    };
-  };
+  data: IndexQuery;
 };
 
-const Index = (props: Props) => (
+const Index = ({ data }: Props) => (
   <Layout>
     <div>
       <h1 className={styles.Header}>Hi people</h1>
       <p>
-        Welcome to your new <strong>{props?.data?.site?.siteMetadata?.title || ''}</strong> site.
+        Welcome to your new <strong>{data?.site?.siteMetadata?.title || ''}</strong> site.
+        <br />
+        url: {data.site?.siteMetadata?.siteUrl || 'unknown'}
       </p>
       <p>Now go build something great.</p>
       <Link to="/page-2/">Go to page 2</Link>
--- src/pages/page-2.tsx
+++ src/pages/page-2.tsx
@@ -1,12 +1,28 @@
 import * as React from 'react';
+import { graphql } from 'gatsby';
 import Link from 'gatsby-link';
 
 import { Layout } from '~/layouts/default';
+import { PageTwoQuery } from '../../types/graphql-types';
 
-const SecondPage = () => (
+export const pageQuery = graphql`
+  query PageTwo {
+    site {
+      siteMetadata {
+        title
+      }
+    }
+  }
+`;
+
+type Props = {
+  data: PageTwoQuery;
+};
+
+const SecondPage = ({ data }: Props) => (
   <Layout>
     <div>
-      <h1>Hi from the second page</h1>
+      <h1>Hi from the second page on {data.site?.siteMetadata?.title}</h1>
       <p>Welcome to page 2</p>
       <Link to="/">Go back to the homepage</Link>
     </div>
